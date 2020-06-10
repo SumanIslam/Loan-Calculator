74abfe7843a47472884bf199c7509d7e749c9a09
suman Islam <sumunahmed1416@gmail.com>
2020-06-11 01:16:13 +0600

    updating calculation and loader

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 document.querySelector('#loan-form').addEventListener('submit', (e) => {
+    document.querySelector('#loading').style.display = 'block';
+    
   // UI variables
-  const monthlyPayment = document.getElementById('monthly-payment');
-  const totalPayment = Document.getElementById('total-payment');
-  const totalInterest = Document.getElementById('total-interest');
+  const monthlyPayment = document.querySelector('#monthly-payment');
+  const totalPayment = document.querySelector('#total-payment');
+  const totalInterest = document.querySelector('#total-interest');
 
-  const principle = parseFloat(document.getElementById('amount').value);
-  const calculatedInterest = parseFloat(document.getElementById('interest').value) / 100 / 12;
-  const calculatedPayments = parseFloat(document.getElementById(years).value) * 12;
+  const principle = parseFloat(document.querySelector('#amount').value);
+  const calculatedInterest = parseFloat(document.querySelector('#interest').value) / 100 / 12;
+  const calculatedPayments = parseFloat(document.querySelector('#years').value) * 12;
 
   // calculate monthly payment
   const x = Math.pow(1 + calculatedInterest, calculatedPayments);
@@ -16,9 +18,36 @@ document.querySelector('#loan-form').addEventListener('submit', (e) => {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * calculatedPayments) - principle).toFixed(2);
-  } else {
 
+    setTimeout(() => {
+      document.querySelector('#loading').style.display = 'none';
+      document.querySelector('#result').style.display = 'block';
+    }, 2000);
+
+  } else {
+    showError('Please check your numbers!');
   }
 
   e.preventDefault();
 })
+
+// show error
+function showError(error) {
+  setTimeout(() => {
+    document.querySelector('#loading').style.display = 'none';
+  }, 2000);
+  const errDiv = document.createElement('div');
+  errDiv.className = 'alert alert-danger';
+  errDiv.appendChild(document.createTextNode(error));
+
+  const card = document.querySelector('.card');
+  const heading = document.querySelector('.heading')
+
+  setTimeout(() => {
+    card.insertBefore(errDiv, heading);
+  },2100)
+
+  setTimeout(() => {
+    document.querySelector('.alert').remove();
+  }, 5000);
+}
